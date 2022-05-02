88e017486d92d9be852106de87d67753414b1c23
rakanshaker <shaker.rakan@gmail.com>
2022-05-01 22:51:18 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import CV from '../src/20210401_RAKANSHAKER_RESUME.pdf'
+import avatarLookingDown from '../src/rakan_8bit_looking_down.svg'
+import avatarEyesClosed from '../src/rakan_8bit_eyes_closed.svg'
 
-function App() {
+const handleBlink = (setState, state) => {
+  setTimeout(() => {
+    setState(!state)
+  }, state ? 250 : 4000);
+};
+
+const App = () => {
+
+  const [isBlinking, setIsBlinking] = useState(false);
+
+  handleBlink(setIsBlinking, isBlinking);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='App'>
+        <div className='leftSide'>
+          <div className='titleContainer'>
+            <p className='rakanshaker'>Rakan Shaker</p>
+            <p className='engineer'>Front End Engineer</p>
+          </div>
+          <div>
+          <a href={CV} download><button className='leftSide__cvButton'>Download My CV</button></a>
+          </div>
+        </div>
+        <div className='rightSide'>
+          <div className='avatarContainer'>
+            <img alt='Rakan!' className='avatarImage' src={isBlinking ? avatarEyesClosed : avatarLookingDown}/>
+          </div>
+          </div>
     </div>
   );
-}
+};
 
 export default App;
